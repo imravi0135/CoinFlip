f13f47f466588f6535c341452c7de6c56a67cba5
ravikishorereddy <ravikishorereddy309@gmail.com>
2022-03-05 15:26:22 +0530

    commit message

---
+++ HeadAndTails/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+int Coin_Head = 1;
+Random random = new Random();
+int coincheck = random.Next(0, 2);
+if (coincheck == Coin_Head)
+{
+    Console.WriteLine("Coin Is Head");
+}
+else
+{
+    Console.WriteLine("Coin Is Tails");
+}
